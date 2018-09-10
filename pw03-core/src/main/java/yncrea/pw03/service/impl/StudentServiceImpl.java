caba25da5e51a57f83fcaba5bc683e49667ea0bf
Thomas Carton <thomas.carton@hei.yncrea.fr>
2018-09-10 10:54:01 +0200

    bla

---
+++ pw03-core/src/main/java/yncrea/pw03/service/impl/StudentServiceImpl.java
@@ -0,0 +1,9 @@
+package yncrea.pw03.service.impl;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Named;
+
+@Named @Transactional
+public class StudentServiceImpl {
+}
